541895a8671a4d96d1da2fdcc38a5aed50e75606
EnzoPeña <enzo.lpge@gmail.com>
2026-02-05 09:34:39 -0500

    Se crea este nuevo proyecto

---
+++ plt_investigador_concursos_prociencia.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 09:30:38 2026
+
+@author: Enzo
+"""
+
+##################################################################################
+# SCRIPT PARA LA CREACIÓN DE UNA BASE DE DATOS SOBRE UN CONJUNTO DE INVESTIGADORES
+##################################################################################
+
+# Se importan las librerias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
