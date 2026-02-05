a643edaf379efb17e1a0eb7ac6830b0550cada00
EnzoPeña <enzo.lpge@gmail.com>
2026-02-05 18:30:48 -0500

    Se fortalece el análisis

--- plt_investigador_concursos_prociencia.py
+++ plt_investigador_concursos_prociencia.py
@@ -161,7 +161,7 @@ merged = merged[['nombre_completo_df1', 'Entidad Actual',
        'Areas|Sub Areas|Disciplinas']]
 
 
-# El dataframe merged se convierte en un archivo xlsx
+# El dataframe merged se convierte en un archivo xlsx, el cual recibe intervención manual con el objetivo de ampliar su información
 merged.to_excel("datos_trabajados.xlsx")
 
 
@@ -172,12 +172,151 @@ cambio = {"codigo_scopus": int_to_str}
 investigador = pd.read_excel("BD_información_investigadores.xlsx", sheet_name="Investigadores", header=0, converters=cambio)
 investigador.columns
 
+# Modifico la columna Area/subareas/disciplina del dataframe investigador
+investigador["area_principal"] = investigador["Areas|Sub Areas|Disciplinas"].str.split("|").str[0].str.split("|").str[0].str.strip()
+
 # Se convierte una columna en una lista
 investigador1 = investigador.dropna(subset=["codigo_scopus"])
 caso = investigador1["codigo_scopus"].tolist()
 type(caso)
 print(caso)
 
+
+# Utilizando la descarga manual realizada de la información almacenada en SCOPUS, se procede a procesar dicha información
+# Es importante señalar que se agrega información de la producción científica de un investigador seleccionado, por eso _2 
+
+publicacion = pd.read_csv("bd_investigadores_seleccionados_2.csv", encoding = "utf-8", delimiter=",")
+
+ 
+ids_set = set(map(str, caso))  # lookup rápido
+
+# 2) Nombre exacto de la columna con IDs en tu DF
+col_ids = "Author(s) ID"
+
+# 3) Pasar de "una fila por publicación" a "una fila por (publicación, autor)"
+df_long = (
+    publicacion
+      .assign(author_id=publicacion[col_ids].fillna("").astype(str).str.split(r"\s*;\s*"))
+      .explode("author_id")
+)
+
+# 4) Limpiar y filtrar solo los IDs de tu lista
+df_long["author_id"] = df_long["author_id"].str.strip()
+df_match = df_long[df_long["author_id"].isin(ids_set)].copy()
+df_match.columns
+
+# Se identifica la cantidad única de investigadores seleccionados asociados con estas publicaciones
+df_match["author_id"].nunique()
+
+#Se cuenta las publicaciones científicas asociadas con un autor típico
+df_match[df_match["author_id"]=="57193778002"].count()
+
+# Se renombran columnas del dataframe df_match
+df_match.rename(columns=({"Document Type":
+                          "Document_type", "author_id":
+                              "codigo_scopus"}), inplace=True)
+
+
+# Se realiza una distribución total de las publicaciones científicas de los autores seleccionados
+df_match.Document_type.value_counts()
+df_match.Document_type.value_counts(normalize=True).round(3)*100
+
+# Se elabora una tabla de contingencia que muestre la cantidad total de publicaciones científicas por año para cada investigador seleccionado
+table1 = pd.pivot_table(df_match, values="EID", index="codigo_scopus", columns="Year", aggfunc="count")
+type(table1)
+table1.reset_index(inplace=True)
+
+
+# Se elabora una tabla de contingencia que muestra la cantidad de producción científica delimitada por año y por cada investigador
+categoria_sele = ["Article", "Review", "Conference paper", "Book chapter"]
+df_match1 = df_match[df_match["Document_type"].isin(categoria_sele)]
+
+table2 = pd.pivot_table(df_match1, values="EID", index="codigo_scopus", columns="Year", aggfunc="count")
+type(table2)
+table2.reset_index(inplace=True)
+
+# Realizo dos fusiones considernado como ancla el código scopus
+fusion1 = pd.merge(investigador, table1, on="codigo_scopus", how="left")
+fusion2 = pd.merge(investigador, table2, on="codigo_scopus", how="left")
+
+# Se construye investigador2
+
+investigador2 = investigador[["codigo_scopus", "nombre_completo_df1"]]
+
+# Se crea un dataframe que muestra la relación entre el investigador y su producción científica total
+produccion1 = df_match[["codigo_scopus", "EID", "DOI", "Title", "Year", "Source title", "Affiliations",
+                             "Language of Original Document", "Funding Details", "Open Access"]]
+
+produccion2 = df_match1[["codigo_scopus", "EID", "DOI", "Title", "Year", "Source title", "Affiliations",
+                             "Language of Original Document", "Funding Details", "Open Access"]]
+
+
+produccion_total = pd.merge(investigador2, produccion1, on="codigo_scopus", how="left")
+produccion_parcial = pd.merge(investigador2, produccion2, on="codigo_scopus", how="left")
+
+# Se delimita tanto la producción total y la producción parcial
+produccion_total = produccion_total[produccion_total["Year"]>=2016]
+produccion_parcial = produccion_parcial[produccion_parcial["Year"]>=2016]
+
+
+a = "BD_investigadores_producción_cientifica_arch"
+
+with pd.ExcelWriter(f'{a}.xlsx') as writer:
+    # Guardar cada DataFrame en una hoja diferente
+    fusion2.to_excel(writer, sheet_name="Investigador", index=False)
+    produccion_parcial.to_excel(writer, sheet_name="Publicaciones", index=False)
+    #df_sin_duplicados.to_excel(writer, sheet_name='2. PubCalificadasRenacytUniv', index=False)
+    #fusion4.to_excel(writer, sheet_name="3.PubClasificadasAfill", index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################################
+# Potencial estrategia en caso pueda usar el API de SCOPUS
+###############################################################################
 
 API_KEY = "7fe684a86517ef163c6e8acd82c787f7"
 
